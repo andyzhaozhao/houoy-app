ca327dec823511d046ce16d5036e54c1f64b8af4
laoniu <939417812@qq.com>
2017-11-22 20:50:37 +0900

    add check is support bluetooth

--- app/src/main/java/gov/smart/health/activity/self/DeviceSettingActivity.java
+++ app/src/main/java/gov/smart/health/activity/self/DeviceSettingActivity.java
@@ -1,6 +1,8 @@
 package gov.smart.health.activity.self;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,6 +28,7 @@ public class DeviceSettingActivity extends AppCompatActivity {
     private Map<String,BleDevice> bleDeviceMap = new HashMap<>();
 
     private static String AddressKey = "addressKey";
+    private static int REQUEST_ENABLE_BLUETOOTH = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,38 +36,64 @@ public class DeviceSettingActivity extends AppCompatActivity {
         findViewById(R.id.btn_setting_device).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BluetoothModule bluetoothModule = BluetoothModule.getInstance();
-                if(bluetoothModule.isBluetoothOpen()) {
-                    String deviceAddress = SharedPreferencesHelper.gettingString(AddressKey,null);
-                    if(deviceAddress != null && bluetoothModule.isConnDevice(getApplicationContext(),deviceAddress)){
-                        SharedPreferencesHelper.settingString(AddressKey,null);
-                        bluetoothModule.disConnectBle();
-                        Toast.makeText(getApplication(), "解除绑定", Toast.LENGTH_LONG).show();
-                    } else {
-                        bluetoothModule.startScanDevice(new ScanDeviceCallback() {
-                            @Override
-                            public void onStartScan() {
-                                Toast.makeText(getApplication(), "开始搜索设备", Toast.LENGTH_LONG).show();
-                            }
-
-                            @Override
-                            public void onScanDevice(BleDevice device) {
-                                bleDeviceMap.put(device.name, device);
-                                Toast.makeText(getApplication(), "搜索设备" + device.name, Toast.LENGTH_LONG).show();
-                            }
-
-                            @Override
-                            public void onStopScan() {
-                                Toast.makeText(getApplication(), "搜索设备结束", Toast.LENGTH_LONG).show();
-                                showDialog();
-                            }
-                        });
-                    }
-                } else {
-                    Toast.makeText(getApplication(), "请确认蓝牙是否正常开启！", Toast.LENGTH_LONG).show();
+                BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
+                if(Bt == null){
+                    Toast.makeText(getApplication(), "您的手机不支持蓝牙功能！", Toast.LENGTH_LONG).show();
+                    return;
                 }
+                boolean btEnable = Bt.isEnabled();
+                if(btEnable){
+                    doConnectionWatch();
+                }else{
+                    Intent btOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(btOn, REQUEST_ENABLE_BLUETOOTH);
+                }
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int ResultCode, Intent date){
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
+            if(Bt.isEnabled()){
+                doConnectionWatch();
+            }
+        }
+    }
+
+    private void doConnectionWatch() {
+        BluetoothModule bluetoothModule = BluetoothModule.getInstance();
+        if(bluetoothModule.isBluetoothOpen()) {
+            String deviceAddress = SharedPreferencesHelper.gettingString(AddressKey,null);
+            if(deviceAddress != null && bluetoothModule.isConnDevice(getApplicationContext(),deviceAddress)){
+                SharedPreferencesHelper.settingString(AddressKey,null);
+                bluetoothModule.disConnectBle();
+                Toast.makeText(getApplication(), "解除绑定", Toast.LENGTH_LONG).show();
+            } else {
+                bluetoothModule.startScanDevice(new ScanDeviceCallback() {
+                    @Override
+                    public void onStartScan() {
+                        Toast.makeText(getApplication(), "开始搜索设备", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onScanDevice(BleDevice device) {
+                        bleDeviceMap.put(device.name, device);
+                        Toast.makeText(getApplication(), "搜索设备" + device.name, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onStopScan() {
+                        Toast.makeText(getApplication(), "搜索设备结束", Toast.LENGTH_LONG).show();
+                        showDialog();
+                    }
+                });
+            }
+        } else {
+            Toast.makeText(getApplication(), "请确认蓝牙是否正常开启！", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void showDialog(){
