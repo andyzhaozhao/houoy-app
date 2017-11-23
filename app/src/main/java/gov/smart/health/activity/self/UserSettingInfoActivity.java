90d1119fdf54bcb84abbd85df2291694665213aa
laoniu <939417812@qq.com>
2017-11-23 21:22:19 +0900

    compass to 80*80

--- app/src/main/java/gov/smart/health/activity/self/UserSettingInfoActivity.java
+++ app/src/main/java/gov/smart/health/activity/self/UserSettingInfoActivity.java
@@ -142,7 +142,7 @@ public class UserSettingInfoActivity extends AppCompatActivity implements EasyPe
                         Gson gson = new Gson();
                         UploadPersonImageInfoModel model = gson.fromJson(response,UploadPersonImageInfoModel.class);
                         if (model.success){
-                            Toast.makeText(getApplication(),"保存失败",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplication(),"保存成功",Toast.LENGTH_LONG).show();
                         } else {
                             Toast.makeText(getApplication(),"保存失败",Toast.LENGTH_LONG).show();
                         }
@@ -170,7 +170,7 @@ public class UserSettingInfoActivity extends AppCompatActivity implements EasyPe
                 c.close();
                 Bitmap bm = BitmapFactory.decodeFile(imagePath);
                 ((ImageView)findViewById(R.id.img_update_user_icon)).setImageBitmap(bm);
-                saveImageToServer(Base64.encodeToString(bitmap2Bytes(bm), Base64.DEFAULT));
+                saveImageToServer(bitmapToString(imagePath));
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -182,5 +182,48 @@ public class UserSettingInfoActivity extends AppCompatActivity implements EasyPe
         bm.compress(Bitmap.CompressFormat.PNG, 30, baos);
         return baos.toByteArray();
     }
+
+
+
+
+    //把bitmap转换成String
+    public static String bitmapToString(String filePath) {
+
+        Bitmap bm = getSmallBitmap(filePath);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 30, baos);
+        byte[] b = baos.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+    // 根据路径获得图片并压缩，返回bitmap用于显示
+    public static Bitmap getSmallBitmap(String filePath) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, 80, 80);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    //计算图片的缩放值
+    public static int calculateInSampleSize(BitmapFactory.Options options,int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height/ (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
+    }
+
 
 }
