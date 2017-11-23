aaa2dbd1abd829b8ed2dd685d71e1e5999ed6ec9
laoniu <939417812@qq.com>
2017-11-23 15:20:26 +0900

    fix login.

--- app/src/main/java/gov/smart/health/activity/self/UserSettingActivity.java
+++ app/src/main/java/gov/smart/health/activity/self/UserSettingActivity.java
@@ -2,6 +2,7 @@ package gov.smart.health.activity.self;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -73,7 +74,7 @@ public class UserSettingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 SharedPreferencesHelper.settingString(SHConstants.LoginUserPkPerson,"");
                 Intent intent = new Intent();
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(IntentCompat.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.setClass(getApplication(), LoginActivity.class);
                 startActivity(intent);
             }
